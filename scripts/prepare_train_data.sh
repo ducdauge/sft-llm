84389e03442351f7b8bb823005d64f1c7a0add15
Edoardo M. Ponti <ep490@cam.ac.uk>
2024-01-29 11:56:37 +0000

    reformat dataset fix

--- scripts/prepare_train_data.sh
+++ scripts/prepare_train_data.sh
@@ -6,4 +6,4 @@ echo "Downloading the gpt4-llm dataset..."
 wget -P data/raw_train/gpt4_alpaca/ https://github.com/Instruction-Tuning-with-GPT-4/GPT-4-LLM/raw/main/data/alpaca_gpt4_data.json
 
 echo "Processing datasets..."
-python reformat_datasets.py --raw_data_dir data/raw_train/ --output_dir data/processed/
+python scripts/reformat_datasets.py --raw_data_dir data/raw_train/ --output_dir data/processed/
